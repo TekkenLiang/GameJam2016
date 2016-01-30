88ec1c4398c2afa62ababe251e3b7332f059b590
tekkenliang <tekken.lty@gmail.com>
2016-01-30 15:41:58 -0800

    Clean up the project
    * Also create player scripts

---
+++ Assets/Scripts/Player/InputControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Player))]
+public class Input : MonoBehaviour {
+
+    protected Player m_player;
+
+	// Use this for initialization
+	void Start () {
+        m_player = GetComponent<Player>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public int m_playerID;
+    //public int getPlayerID() { return m_playerID; }
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
