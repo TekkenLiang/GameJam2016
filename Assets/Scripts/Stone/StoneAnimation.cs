51c017bd6a1eee4f764e6d7676aed5e41d88f071
AtwoodDeng <dengjdtsinghua@gmail.com>
2016-01-30 18:38:37 -0800

    add character & stone in the test scene

---
+++ Assets/Scripts/Stone/StoneAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class StoneAnimation : MonoBehaviour {
+
+	[SerializeField] List<SpriteRenderer> shadowList;
+	[SerializeField] float shadowLoopTime = 2f;
+	void Awake()
+	{
+		foreach(SpriteRenderer shadow in shadowList)
+		{
+			shadow.transform.DOScale(shadow.transform.localScale * 0.5f, shadowLoopTime + Random.Range(-0.3f, 0.3f))
+			.SetLoops(9999,LoopType.Yoyo)
+			.SetDelay(Random.Range(0, shadowLoopTime));
+		}
+	}
+
+
+}
