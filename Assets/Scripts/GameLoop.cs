abd4f233beda530c77b13ea4242ae220f047eaf3
Rin <Dianna@usc-secure-wireless-036-039.usc.edu>
2016-01-30 16:16:00 -0800

    GameLoop

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class GameLoop : MonoBehaviour {
 
+	//timer
 	public float timeLimit = 0;
-
 	[SerializeField]
 	float timer = 0;
 
 	public int totalTask = 5;
 
+	//Players
 	public GameObject playerPrefab;
 	public GameObject player1;
 	public Vector2 player1Pos;
@@ -17,6 +18,7 @@ public class GameLoop : MonoBehaviour {
 	public GameObject player2;
 	public Vector2 player2Pos;
 
+	public GameObject grids;
 
 	// Use this for initialization
 	void Start () {
@@ -26,9 +28,9 @@ public class GameLoop : MonoBehaviour {
 
 		//initialize grid
 
+
 		//spawn player
 		setupPlayers();
-
 
 		//start music control
 
@@ -36,23 +38,38 @@ public class GameLoop : MonoBehaviour {
 
 	void setupPlayers()
 	{
-		
 		GameObject player1 = (GameObject) Instantiate(playerPrefab, player1Pos, transform.rotation);
+		player1.name = "Player 1";
 		PlayerStatus PS1 = player1.GetComponent<PlayerStatus>();
 		PS1.setupPlayerStatus(1 ,totalTask);
 
 		GameObject player2 = (GameObject) Instantiate(playerPrefab, player2Pos, transform.rotation);
+		player1.name = "Player 2";
 		PlayerStatus PS2 = player2.GetComponent<PlayerStatus>();
 		PS2.setupPlayerStatus(2 ,totalTask);
 	}
 
+	void endgame()
+	{
+		Debug.Log("GAME END");
+		//play sound
 
+		//freeze grid
 
+		//animation
 
+		//fade out
 
+		//play final music
 
+	}
+		
 	// Update is called once per frame
 	void Update () {
-		
+		timer -= Time.deltaTime;
+		if(timer < 0)
+		{
+			endgame();
+		}
 	}
 }
