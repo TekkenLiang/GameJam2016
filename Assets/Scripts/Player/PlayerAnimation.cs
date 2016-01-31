82c1657b3adc8d7c1c236b364922699db3a1810c
AtwoodDeng <dengjdtsinghua@gmail.com>
2016-01-30 23:10:45 -0800

    animation & test script for characters

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	[SerializeField] Animator myAnimatior;
+	void Awake()
+	{
+		if (myAnimatior == null )
+		{
+			myAnimatior = GetComponent<Animator>();
+		}
+
+	}
+
+	void SetLeft()
+	{
+		transform.localScale = new Vector3(-1f,1f,1f);
+	}
+
+	void SetRight()
+	{
+		transform.localScale = new Vector3(1f,1f,1f);
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			JumpRight();
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			JumpLeft();
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			JumpUp();
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			JumpDown();
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Fail();
+		}
+	}
+
+	public void JumpRight()
+	{
+		myAnimatior.SetBool("jump",true);
+		SetRight();
+	}
+
+	public void Fail()
+	{
+		myAnimatior.SetBool("fail",true);
+	}
+
+	public void JumpLeft()
+	{
+		myAnimatior.SetBool("jump",true);
+		SetLeft();
+	}
+
+	public void JumpUp()
+	{
+		myAnimatior.SetBool("jump",true);
+	}
+
+	public void JumpDown()
+	{
+		myAnimatior.SetBool("jump",true);
+	}
+
+}
