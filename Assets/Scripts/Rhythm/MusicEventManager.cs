abbab80c74ee340c7d6707fae885b04696c7e70d
Daniyal Mirza <dmirza@usc.edu>
2016-01-31 02:34:08 -0800

    MusicCore
    Play Tracks for multiple Players

---
+++ Assets/Scripts/Rhythm/MusicEventManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicEventManager : MonoBehaviour 
+{
+
+	public delegate void GameEvent();
+	public static event GameEvent GameStart;
+	public static event GameEvent GameEnd;
+
+	public delegate void PlayerEvent(PlayerID playerID);
+	public static event PlayerEvent LevelProgression;
+
+	public static void StartGame()
+	{
+		if(GameStart != null)
+		{
+			GameStart();
+		}
+	}
+
+	public static void EndGame()
+	{
+		if(GameEnd != null)
+		{
+			GameEnd();
+		}
+	}
+
+	public static void MoveToNextLevel(PlayerID playerID)
+	{
+		if (LevelProgression != null)
+		{
+			LevelProgression(playerID);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Rhythm/PlayerTracks.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTracks : MonoBehaviour {
+
+	[SerializeField] 
+	private AudioClip[] AudioClipList;
+
+	private int AudioClipListLength = -1;
+
+	public int TrackListLength
+	{
+		get { return AudioClipListLength; }
+	}
+
+	void Awake()
+	{
+		AudioClipListLength = AudioClipList.Length; 
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	public AudioClip GetClip(int index)
+	{
+		if (index < 0 || index >= AudioClipListLength)
+		{
+			Debug.LogError("Invalid index fro Clip Requested");
+			return null;
+		}
+
+		return AudioClipList[index];
+	}
+
+	
+
+}
